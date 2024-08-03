ec51b5a35d075b7f583b1e96c88a6331e8a9228c
yousseflach <lachgar.youssef6174@gmail.com>
2024-08-03 16:31:25 +0100

    upd: Application class

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -18,7 +18,7 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		logger.info("Application executed...");
+		logger.info("Application executed success...");
 		SpringApplication.run(Application.class, args);
 	}
 
